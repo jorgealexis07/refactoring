a11b9eac5ee692b4a75fe24e71597e202505ae77
jorgealexis07 <201711104@tese.edu.mx>
2022-05-05 16:37:58 -0500

    Se corrige la exportacion  reader.js

---
+++ app/service/ExplorerService.js
@@ -0,0 +1,17 @@
+const Reader = require("../utils/Reader");
+const readExplorers = Reader.getreadJsonFile("explorers.json");
+
+class ExplorerService{
+    
+    static filterByMission(readExplorers, mission){
+        const explorersInNode = readExplorers.filter((explorer) => explorer.mission == mission);
+        return explorersInNode;
+    }
+    
+    
+}
+exports.data = ExplorerService;
+
+ExplorerService.filterByMission(readExplorers,"node");
+//ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+//ExplorerService.getExplorersUsernamesByMission(explorers, "node");
