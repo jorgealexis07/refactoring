9715e0af6f05bbad0eeee2700df3f0b853572f5c
jorgealexis07 <201711104@tese.edu.mx>
2022-05-05 22:50:31 -0500

    creacion de losmetodos en la clace

--- app/service/ExplorerService.js
+++ app/service/ExplorerService.js
@@ -7,7 +7,16 @@ class ExplorerService{
         const explorersInNode = readExplorers.filter((explorer) => explorer.mission == mission);
         return explorersInNode;
     }
-    
+    static getAmountOfExplorersByMission(readExplorers, mission){
+        const explorersInNode = readExplorers.filter((explorer) => explorer.mission == mission);
+        var cantExpl = explorersInNode.length;
+        return cantExpl;
+    }
+    static getExplorersUsernamesByMission(explorers, mission){
+        const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
+        const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
+        return usernamesInNode;
+    }
     
 }
 module.exports = ExplorerService;
