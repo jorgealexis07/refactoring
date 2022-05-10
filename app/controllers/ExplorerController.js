17ff8dac923a8c1ac1af652eafc6590e141b2950
jorgealexis07 <201711104@tese.edu.mx>
2022-05-10 18:29:04 -0500

    get explorers usernames  by mission

--- app/controllers/ExplorerController.js
+++ app/controllers/ExplorerController.js
@@ -1,15 +1,21 @@
 const ExplorerServ = require("./../../app/service/ExplorerService");
 const FizzbuzzServ = require("./../../app/service/FizzbuzzService");
 const Reader = require("./../../app/utils/Reader");
-
+var read = Reader.getreadJsonFile("explorers.json")
 class ExplorerController{
     static getExplorersByMission(mission){
-        var read = Reader.getreadJsonFile("explorers.json")
         var filterforMission = ExplorerServ.filterByMission(read,mission)
-        
         return filterforMission;
     }
+
+    static getExplorersUsernamesByMission(mission){
+        var getusernamebymission = ExplorerServ.getExplorersUsernamesByMission(read, mission);
+        return getusernamebymission
+    }
+
+
 }
 module.exports = ExplorerController;
 
-console.log(ExplorerController.getExplorersByMission("node"));
+//console.log(ExplorerController.getExplorersByMission("node"));
+console.log(ExplorerController.getExplorersUsernamesByMission("node"));
