9d296e92d322279c2dd80b1942afc7651f690924
jorgealexis07 <201711104@tese.edu.mx>
2022-05-10 14:15:10 -0500

    Adding automated test in github actions

--- test/service/ExplorerService.test.js
+++ test/service/ExplorerService.test.js
@@ -9,7 +9,6 @@ describe("Test for Explorer service", () => {
         //Aqui invocas el codigo que vas a usar en tu app
         const explorers = Reader.getreadJsonFile("explorers.json"); 
         const explorer = ExplorerService.filterByMission(explorers,"node") 
-     
         expect(explorer.mission).toBe("node")
     } )
 
