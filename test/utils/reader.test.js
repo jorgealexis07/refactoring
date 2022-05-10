539f336c893265c05b7afc3f5b1b0cc7181ace21
jorgealexis07 <201711104@tese.edu.mx>
2022-05-10 15:04:15 -0500

    falla en reder

--- test/utils/reader.test.js
+++ test/utils/reader.test.js
@@ -2,11 +2,7 @@ const ReaderFile = require("../../app/utils/reader")
 
 describe("Test for reader", () => {
 
-    test('1. Prueba de lector de archivo explorers.json', () => {
-        const reader = ReaderFile.getreadJsonFile("explorers.json")
-        expect(reader.path).toBe("explorers.json")
-    } )
-
+  
     
     
 
