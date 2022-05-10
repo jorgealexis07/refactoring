2bad506f2454c9b69020d449399f6382e94db374
jorgealexis07 <201711104@tese.edu.mx>
2022-05-09 23:37:25 -0500

    Implementacion de lectura del json en la vista

---
+++ app/views/view.js
@@ -0,0 +1,21 @@
+// Clase anterior con la que obtenemos los explorers
+const Reader = require("../utils/Reader");
+const explorers = Reader.getreadJsonFile("./../utils/explorers.json");
+
+// Aplicación del ExplorerService sobre la lista de explorers
+ExplorerService.filterByMission(explorers, "node");
+ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+
+const explorer1 = {name: "Explorer1", score: 1}
+FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
