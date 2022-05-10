436e352b3e7c1f4b29a125fa8ca6503fab3c81a3
jorgealexis07 <201711104@tese.edu.mx>
2022-05-10 15:23:39 -0500

    Quito prueba explorer service

--- test/service/ExplorerService.test.js
+++ test/service/ExplorerService.test.js
@@ -1,16 +1,3 @@
-const ExplorerService = require("./../../app/service/ExplorerService")
-const Reader = require("./../../app/utils/Reader")
-
-
-
-describe("Test for Explorer service", () => {
-
-    test('1. Create a new user using the ExplorerService', () => {
-        //Aqui invocas el codigo que vas a usar en tu app
-        const explorers = Reader.getreadJsonFile("explorers.json"); 
-        const explorer = ExplorerService.filterByMission(explorers,"node") 
-        expect(explorer.mission).toBe("node")
-    } )
 
 
     
