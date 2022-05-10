6be55457d1158d909228a7dc03a156ff0e820826
jorgealexis07 <201711104@tese.edu.mx>
2022-05-10 15:11:22 -0500

    quito una prueba

--- test/service/ExplorerService.test.js
+++ test/service/ExplorerService.test.js
@@ -12,12 +12,6 @@ describe("Test for Explorer service", () => {
         expect(explorer.mission).toBe("node")
     } )
 
-    test('1. Create a new user using the ExplorerService', () => {
-        //Aqui invocas el codigo que vas a usar en tu app
-        const explorers = Reader.getreadJsonFile("explorers.json"); 
-        const explorer = ExplorerService.filterByMission(explorers,"node") 
-        expect(explorer.mission).toBe("node")
-    } )
 
     
 
