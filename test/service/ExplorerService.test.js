bd9435ae3474d9abcd72031b541d799e0e18b66e
jorgealexis07 <201711104@tese.edu.mx>
2022-05-11 12:21:17 -0500

    test of the calculate for the explorers in mission java

--- test/service/ExplorerService.test.js
+++ test/service/ExplorerService.test.js
@@ -5,13 +5,19 @@ const Reader = require("./../../app/utils/Reader");
 
 describe("Test for Explorer service", () => {
 
-    test("1. Create a new user using the ExplorerService", () => {
+    test("1. Calculated explorers number using the ExplorerService in node", () => {
         //Aqui invocas el codigo que vas a usar en tu app
         const explorers = Reader.getreadJsonFile("explorers.json"); 
         const explorer = ExplorerService.filterByMission(explorers,"node"); 
         expect(explorer.length).toBe(10); 
     } );
 
+    test("2. Calculated explorers number using the ExplorerService in java", () => {
+        //Aqui invocas el codigo que vas a usar en tu app
+        const explorers = Reader.getreadJsonFile("explorers.json"); 
+        const explorer = ExplorerService.filterByMission(explorers,"java"); 
+        expect(explorer.length).toBe(5); 
+    } );
     
     
 
